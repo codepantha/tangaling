c10c96696b2d57948994d18235205f1808428dd7
codepanther <pezechigozirim@gmail.com>
2022-12-12 18:43:03 +0100

    Tell Devise to use the polished email layout

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,9 @@ module Tangaling
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.to_prepare do
+      Devise::Mailer.layout "mailer"
+    end
   end
 end
