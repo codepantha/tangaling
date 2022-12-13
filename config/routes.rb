36ef1cabc0e7a8164cc106583958ee366e218acb
codepanther <pezechigozirim@gmail.com>
2022-12-13 16:55:23 +0100

    Create TimeslinesController and register the path in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  authenticate :user do
+    resources :timelines,
+      only: [:index, :show]
+  end
+
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
