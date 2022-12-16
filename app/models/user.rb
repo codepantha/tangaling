fbdef22f354b9123935645caf60f787b4a662028
codepanther <pezechigozirim@gmail.com>
2022-12-16 17:17:05 +0100

    Display posts on member timeline

--- app/models/user.rb
+++ app/models/user.rb
@@ -68,6 +68,21 @@ class User < ApplicationRecord
     username
   end
 
+  def name
+    if last_name
+      "#{first_name} #{last_name}"
+    else
+      first_name
+    end
+  end
+
+  def profile_picture_url
+    @profile_picture_url ||= begin
+      hash = Digest::MD5.hexdigest(email)
+      "https://www.gravatar.com/avatar/#{hash}?d=wavatar"
+    end
+  end
+
   def login
     @login || username || email
   end
